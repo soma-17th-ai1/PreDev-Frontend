5bb161d124b4707ae891c36e8808ed11e9fa3d6a
이병윤 <69826182+zihasoo@users.noreply.github.com>
2026-05-14 23:40:09 +0900

    fix: fetch를 캐시하는 모노가타리의 기본 동작을 지웠습니다. (#12)

--- service-worker.js
+++ service-worker.js
@@ -71,40 +71,40 @@ self.addEventListener ('activate', (event) => {
 	return self.clients.claim ();
 });
 
-self.addEventListener ('fetch', (event) => {
+// self.addEventListener ('fetch', (event) => {
 
-	if (event.request.method !== 'GET') {
-		return;
-	}
+// 	if (event.request.method !== 'GET') {
+// 		return;
+// 	}
 
-	event.respondWith (
-		caches.match (event.request).then ((cached) => {
-			function fetchedFromNetwork (response) {
-				const cacheCopy = response.clone ();
+// 	event.respondWith (
+// 		caches.match (event.request).then ((cached) => {
+// 			function fetchedFromNetwork (response) {
+// 				const cacheCopy = response.clone ();
 
-				caches.open (`${name}-v${version}`).then (function add (cache) {
-					cache.put (event.request, cacheCopy);
-				});
-				return response;
-			}
+// 				caches.open (`${name}-v${version}`).then (function add (cache) {
+// 					cache.put (event.request, cacheCopy);
+// 				});
+// 				return response;
+// 			}
 
-			function unableToResolve () {
-				return new Response (`
-					<!DOCTYPE html><html lang=en><title>Bad Request</title><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><style>body,html{width:100%;height:100%}body{text-align:center;color:#545454;margin:0;display:flex;justify-content:center;align-items:center;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Fira Sans","Droid Sans","Helvetica Neue",sans-serif}h1,h2{font-weight:lighter}h1{font-size:4em}h2{font-size:2em}</style><div><h1>Service Unavailable</h1><h2>Sorry, the server is currently unavailable or under maintenance, try again later.</h2></div>
-				`, {
-					status: 503,
-					statusText: 'Service Unavailable',
-					headers: new Headers ({
-						'Content-Type': 'text/html'
-					})
-				});
-			}
+// 			function unableToResolve () {
+// 				return new Response (`
+// 					<!DOCTYPE html><html lang=en><title>Bad Request</title><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><style>body,html{width:100%;height:100%}body{text-align:center;color:#545454;margin:0;display:flex;justify-content:center;align-items:center;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Fira Sans","Droid Sans","Helvetica Neue",sans-serif}h1,h2{font-weight:lighter}h1{font-size:4em}h2{font-size:2em}</style><div><h1>Service Unavailable</h1><h2>Sorry, the server is currently unavailable or under maintenance, try again later.</h2></div>
+// 				`, {
+// 					status: 503,
+// 					statusText: 'Service Unavailable',
+// 					headers: new Headers ({
+// 						'Content-Type': 'text/html'
+// 					})
+// 				});
+// 			}
 
-			const networked = fetch (event.request)
-				.then (fetchedFromNetwork, unableToResolve)
-				.catch (unableToResolve);
+// 			const networked = fetch (event.request)
+// 				.then (fetchedFromNetwork, unableToResolve)
+// 				.catch (unableToResolve);
 
-			return cached || networked;
-		})
-	);
-});
+// 			return cached || networked;
+// 		})
+// 	);
+// });
